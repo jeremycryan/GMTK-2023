75c45510b3e805c38a2b7ff19b49ce041d6d854b
Paul <superduperpacman42@gmail.com>
2023-07-08 11:10:09 -0400

    Aiming and laser sight

--- constants.py
+++ constants.py
@@ -13,9 +13,18 @@ TILE_SIZE = TILE_WIDTH, TILE_HEIGHT
 # Physics parameters
 GRAVITY = 300                   # pixels/s^2
 DRAG = 0.1                      # 1/s
-RESTITUTION = 0.15               # unitless
-FRICTION = 0.7                  # unitless
 V_MIN_BOUNCE = 25               # pixels/s
+RESTITUTION = 0.15              # unitless
+TANGENTIAL_RESITUTION = 0.7     # unitless
+
+FRICTION = 10                   # 1/s
+V_MIN_SLIDE = 5                 # pixels/s
 
 # Zombie parameters
-ZOMBIE_SPEED = 5              # pixels/s
+ZOMBIE_SPEED = 5                # pixels/s
+
+# Hero parameters
+RECOIL = 50                     # pixels/s
+COOLDOWN = 1                    # s
+TARGET_PRIORITY = 0.7           # unitless
+SWIVEL_SPEED = 1                # rad/s
--- frame.py
+++ frame.py
@@ -3,6 +3,7 @@ import random
 import pygame
 
 from grid import Grid, Tile
+from hero import Hero
 from zombie import Zombie
 
 
@@ -14,19 +15,24 @@ class Frame:
     def load(self):
         self.grid = Grid()
         self.zombies = []
+        self.heros = [Hero(self, 900, 450)]
 
     def update(self, dt, events):
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.pos)
                 self.zombies.append(Zombie(self, *event.pos))
         self.grid.update(dt, events)
+        for hero in self.heros:
+            hero.update(dt, events)
         for zombie in self.zombies:
             zombie.update(dt, events)
-        pass
 
     def draw(self, surface, offset=(0, 0)):
         surface.fill((0, 0, 0))
         self.grid.draw(surface, offset, only=[Tile.AIR])
+        for hero in self.heros:
+            hero.draw(surface, offset)
         for zombie in self.zombies:
             zombie.draw(surface, offset)
         self.grid.draw(surface, offset, only=[Tile.GROUND])
