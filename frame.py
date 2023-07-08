43612f4a770207eaf87fab0f6eb19748b30d599d
jeremycryan <jeremychristopherryan@gmail.com>
2023-07-07 20:17:19 -0500

    Initial commit

---
+++ frame.py
@@ -0,0 +1,18 @@
+import random
+
+class Frame:
+    def __init__(self, game):
+        self.game = game
+        self.done = False
+
+    def load(self):
+        pass
+
+    def update(self, dt, events):
+        pass
+
+    def draw(self, surface, offset=(0, 0)):
+        surface.fill((0, 0, 0))
+
+    def next_frame(self):
+        return Frame(self.game)
---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame
+import asyncio
+import sys
+
+import contants as c
+import frame as f
+from image_manager import ImageManager
+from sound_manager import SoundManager
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(c.WINDOW_SIZE)
+        self.clock = pygame.time.Clock()
+        SoundManager.init()
+        ImageManager.init()
+
+        asyncio.run(self.main())
+
+    async def main(self):
+        current_frame = f.Frame(self)
+        current_frame.load()
+        self.clock.tick(60)
+
+        while True:
+            dt, events = self.get_events()
+            await asyncio.sleep(0)
+            if dt == 0:
+                dt = 1/100000
+            pygame.display.set_caption(f"{c.CAPTION} ({int(1/dt)} FPS)")
+            if dt > 0.05:
+                dt = 0.05
+            current_frame.update(dt, events)
+            await asyncio.sleep(0)
+            current_frame.draw(self.screen, (0, 0))
+            pygame.display.flip()
+
+            if current_frame.done:
+                current_frame = current_frame.next_frame()
+                current_frame.load()
+
+    def get_events(self):
+        dt = self.clock.tick(c.FPS)/1000
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        return dt, events
+
+
+if __name__=="__main__":
+    Game()
